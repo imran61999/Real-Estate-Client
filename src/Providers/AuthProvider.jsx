a98df49d5d4acddaabc43673b8e52442c841c293
imran61999 <smemrannit@gmail.com>
2024-04-13 23:49:06 +0600

    swiper slider added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -3,8 +3,7 @@ import { createContext } from "react";
 import app from "../firebase/firebase.init";
 
 
-
-export const AuthContext  = createContext(null)
+export const AuthContext  = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
